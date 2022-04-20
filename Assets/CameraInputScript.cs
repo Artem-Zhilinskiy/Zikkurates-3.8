121f21b315508ecf4f8784f26c284154ce046fc3
Artem-Zhilinskiy <90002515+Artem-Zhilinskiy@users.noreply.github.com>
2022-04-20 22:46:55 +0300

    20.04.2022
    Делаю перемещение камеры.. Надо сделать перемещение не через AddForce, а через корутину

---
+++ Assets/CameraInputScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Zikkurat
+{
+    public class CameraInputScript : MonoBehaviour
+    {
+        public PlayerControls controls;
+
+        private void Awake()
+        {
+            controls = new PlayerControls();
+        }
+
+        private void OnEnable()
+        {
+            controls.ActionMap.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.ActionMap.Disable();
+        }
+        private void Update()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            var value = controls.ActionMap.CameraMovement.ReadValue<Vector2>();
+            Vector3 direction = new Vector3(value.x,0 , value.y);
+            GetComponent<Rigidbody>().AddForce(direction, ForceMode.Force);
+        }
+    }
+}
