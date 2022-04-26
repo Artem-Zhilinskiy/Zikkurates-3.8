3a437a215782c1b073816e23d5c755bb5c46f84f
Artem-Zhilinskiy <Arterm313132@gmail.com>
2022-04-26 21:45:12 +0300

    26.04.2022
    Сделал вращение камеры, теперь надо делать панели при щелчке мыши на ворота.
    Сделать три разные панели с анимацией, отображаться в данный момент времени должна только одна.

--- Assets/CameraInputScript.cs
+++ Assets/CameraInputScript.cs
@@ -11,6 +11,9 @@ namespace Zikkurat
         Coroutine CoroutineMovement;
         private Transform _cameraTransform;
 
+        [SerializeField, Tooltip("Скорость вращения камеры")]
+        private float _speedRotate = 20f;
+
         private void Awake()
         {
             controls = new PlayerControls();
@@ -21,13 +24,40 @@ namespace Zikkurat
         private void OnEnable()
         {
             controls.ActionMap.Enable();
-            controls.ActionMap.RotateCamera.performed += RotateCamera();
         }
 
         private void OnDisable()
         {
             controls.ActionMap.Disable();
-            controls.ActionMap.RotateCamera.performed -= RotateCamera();
+        }
+
+        private void Update()
+        {
+            if (Mouse.current.rightButton.isPressed)
+            {
+                //Debug.Log("правая кнопка мыши нажата и удерживается");
+                var delta = controls.ActionMap.RotateCamera.ReadValue<Vector2>();
+                var angle = new Vector3(-delta.y, delta.x, 0f) * _speedRotate * Time.deltaTime;
+                //Вращаем камеру
+                UpdateRotateWithCorrection(transform.rotation * Quaternion.Euler(angle));
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }       
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        //Обновляет поворот камеры без уклона по оси OZ
+        private void UpdateRotateWithCorrection(in Quaternion rotate)
+        {
+            transform.localRotation = rotate;
+
+            var corrective = transform.eulerAngles;
+            corrective.z = 0f;
+            transform.eulerAngles = corrective;
         }
 
         private IEnumerator Movement()
