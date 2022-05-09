8df623a33231218514b8a2185095ce18823c34b8
Artem-Zhilinskiy <Arterm313132@gmail.com>
2022-05-09 21:54:50 +0300

    09.05.2022
    Создание юнита

---
+++ Assets/Scripts/UnitManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Zikkurat
+{
+    public class UnitManager : MonoBehaviour
+    {
+        [SerializeField, Header("Префабы бойцов")]
+        public GameObject GreenFighter;
+        public GameObject RedFighter;
+        public GameObject BlueFighter;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            /*
+            Instantiate(GreenFighter, new Vector3(0, 30, 0), Quaternion.identity);
+            Debug.Log("Создан тестовый воин");
+            */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
