e97b528dfb2fbea57e4a7ebaf479f4c3c6216b5e
Artem-Zhilinskiy <Arterm313132@gmail.com>
2022-05-18 20:18:55 +0300

    18.05.2022
    Разбираюсь в SteeringBehaviours

--- Assets/Scripts/BaseUnit.cs
+++ Assets/Scripts/BaseUnit.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseUnit : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Zikkurat
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public abstract class BaseUnit : MonoBehaviour
     {
-        
+        private Rigidbody _rigidBody;
+
+        public BaseUnit Target { get; set; }
+
+        public SteeringBehaviourData GetSteeringBehaviorData {get; private set;}
+
+        public float Mass => _rigidBody.mass;
+
+        protected virtual void Start()
+        {
+            _rigidBody = GetComponent<RigidBody>();
+            GetSteeringBehaviorData = ConfiguraionManager.Self.GetSteeringBehaviourData;
+        }
+
+        public Vector3 GetVelocity()
+        {
+            return _rigidBody.velocity;
+        }
+
+        public void SetVelocity (Vector3 velocity)
+        {
+            _rigidBody.velocity = velocity;
+        }    
     }
 }
